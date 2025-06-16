b315f9fa581b962b464ac6bfe05bea40e9db9c63
andres <andres.calderon.sacarias@gmail.com>
2025-06-15 18:38:35 -0600

    se genera el componente card base para la feature productos

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { ProductsCard } from "./features/products/pages/products-card/products-card";
 
 @Component({
   selector: 'app-root',
-  imports: [MatSidenavModule, MatToolbarModule,MatListModule, MatIconModule],
+  imports: [MatSidenavModule, MatToolbarModule, MatListModule, MatIconModule, ProductsCard],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/features/products/pages/products-card/products-card.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-products-card',
+  imports: [MatCardModule],
+  templateUrl: './products-card.html',
+  styleUrl: './products-card.scss'
+})
+export class ProductsCard {
+
+}
