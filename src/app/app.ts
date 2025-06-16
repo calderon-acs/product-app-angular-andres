f243c80357e29a398128db1e5e7d274c961fef2c
andres <andres.calderon.sacarias@gmail.com>
2025-06-15 18:30:28 -0600

    se crea el dashbord principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [MatSidenavModule, MatToolbarModule,MatListModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
